4c1e04a5345aed914867e70b4b323b356f92506b
sacolle <pedro.h.b.colle@gmail.com>
2025-11-13 12:49:09 -0300

    bug fix de bounds incorretas na inicialização da zona de absorção

--- original/boundary.c
+++ original/boundary.c
@@ -5,9 +5,9 @@
 
 
 void RandomVelocityBoundary(int sx, int sy, int sz,
-			    int nx, int ny, int nz,
-			    int bord, int absorb,
-			    float *vpz, float *vsv) {
+                int nx, int ny, int nz,
+                int bord, int absorb,
+                float *vpz, float *vsv) {
 
   int i, ix, iy, iz;
   int distx, disty, distz, dist;
@@ -22,8 +22,8 @@ void RandomVelocityBoundary(int sx, int sy, int sz,
   for (iz=bord+absorb; iz<nz+bord+absorb; iz++) {
     for (iy=bord+absorb; iy<ny+bord+absorb; iy++) {
       for (i=ind(bord+absorb,iy,iz); i<ind(nx+bord+absorb,iy,iz); i++) {
-	maxP=fmaxf(vpz[i],maxP);
-	maxS=fmaxf(vsv[i],maxS);
+    maxP=fmaxf(vpz[i],maxP);
+    maxS=fmaxf(vsv[i],maxS);
       }
     }
   }
@@ -35,63 +35,63 @@ void RandomVelocityBoundary(int sx, int sy, int sz,
   for (iz=0; iz<sz; iz++) {
     for (iy=0; iy<sy; iy++) {
       for (ix=0; ix<sx; ix++) {
-	i=ind(ix,iy,iz);
-	// do nothing inside input grid
-	if ((iz>=firstIn && iz<=bordLen+nz) &&
-	    (iy>=firstIn && iy<=bordLen+ny) &&
-	    (ix>=firstIn && ix<=bordLen+nx)) {
-	  continue;
-	}
-	// random speed inside absortion zone
-	else if ((iz>=bord && iz<=2*bordLen+nz) &&
-		 (iy>=bord && iy<=2*bordLen+ny) &&
-		 (ix>=bord && ix<=2*bordLen+nx)) {
-	  if (iz>bordLen+nz) {
-	    distz=iz-bordLen-nz;
-	    ivelz=bordLen+nz;
-	  } else if (iz<firstIn) {
-	    distz=firstIn-iz;
-	    ivelz=firstIn;
-	  } else {
-	    distz=0;
-	    ivelz=iz;
-	  }
-	  if (iy>bordLen+ny) {
-	    disty=iy-bordLen-ny;
-	    ively=bordLen+ny;
-	  } else if (iy<firstIn) {
-	    disty=firstIn-iy;
-	    ively=firstIn;
-	  } else {
-	    disty=0;
-	    ively=iy;
-	  }
-	  if (ix>bordLen+nx) {
-	    distx=ix-bordLen-nx;
-	    ivelx=bordLen+nx;
-	  } else if (ix<firstIn) {
-	    distx=firstIn-ix;
-	    ivelx=firstIn;
-	  } else {
-	    distx=0;
-	    ivelx=ix;
-	  }
-	  dist=(disty>distz)?disty:distz;
-	  dist=(dist >distx)?dist :distx;
-	  bordDist=(float)(dist)*frac;
-	  rfac=(float)rand()/(float)RAND_MAX;
-	  vpz[i]=vpz[ind(ivelx,ively,ivelz)]*(1.0-bordDist)+
-	    maxP*rfac*bordDist;
-	  vsv[i]=vsv[ind(ivelx,ively,ivelz)]*(1.0-bordDist)+
-	    maxS*rfac*bordDist;
-	}
-	// null speed at border
-	else
-	//PPL added {} surrounding vpz and vsv lines below
-	{
-	  vpz[i]=0.0;
-	  vsv[i]=0.0;
-	}
+    i=ind(ix,iy,iz);
+    // do nothing inside input grid
+    if ((iz>=firstIn && iz<=bordLen+nz) &&
+        (iy>=firstIn && iy<=bordLen+ny) &&
+        (ix>=firstIn && ix<=bordLen+nx)) {
+      continue;
+    }
+    // random speed inside absortion zone
+    else if ((iz >= bord && iz <= bord + absorb + nz + absorb - 1) &&
+             (iy >= bord && iy <= bord + absorb + ny + absorb - 1) &&
+             (ix >= bord && ix <= bord + absorb + nx + absorb - 1)) {
+      if (iz>bordLen+nz) {
+        distz=iz-bordLen-nz;
+        ivelz=bordLen+nz;
+      } else if (iz<firstIn) {
+        distz=firstIn-iz;
+        ivelz=firstIn;
+      } else {
+        distz=0;
+        ivelz=iz;
+      }
+      if (iy>bordLen+ny) {
+        disty=iy-bordLen-ny;
+        ively=bordLen+ny;
+      } else if (iy<firstIn) {
+        disty=firstIn-iy;
+        ively=firstIn;
+      } else {
+        disty=0;
+        ively=iy;
+      }
+      if (ix>bordLen+nx) {
+        distx=ix-bordLen-nx;
+        ivelx=bordLen+nx;
+      } else if (ix<firstIn) {
+        distx=firstIn-ix;
+        ivelx=firstIn;
+      } else {
+        distx=0;
+        ivelx=ix;
+      }
+      dist=(disty>distz)?disty:distz;
+      dist=(dist >distx)?dist :distx;
+      bordDist=(float)(dist)*frac;
+      rfac=(float)rand()/(float)RAND_MAX;
+      vpz[i]=vpz[ind(ivelx,ively,ivelz)]*(1.0-bordDist)+
+        maxP*rfac*bordDist;
+      vsv[i]=vsv[ind(ivelx,ively,ivelz)]*(1.0-bordDist)+
+        maxS*rfac*bordDist;
+    }
+    // null speed at border
+    else
+    //PPL added {} surrounding vpz and vsv lines below
+    {
+      vpz[i]=0.0;
+      vsv[i]=0.0;
+    }
       }
     }
   }
